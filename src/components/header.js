4b723f5cf356c1e9765a972468a3250ba7847788
Yoidea <yoidea.cc@gmail.com>
2019-12-15 04:14:29 +0900

    feat: 🎸 Create top page

--- src/components/header.js
+++ src/components/header.js
@@ -3,31 +3,22 @@ import PropTypes from "prop-types";
 import React from "react";
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
+  <header>
+    <nav
+      className="navbar is-transparent is-dark is-fixed-top is-hidden-touch"
+      style={{ backgroundColor: "rgba(0,0,0,0.3)" }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+      <div className="navbar-menu container">
+        <div className="navbar-start">
+          <Link className="navbar-item" to="/">
+            Home
+          </Link>
+          <Link className="navbar-item" to="/dynmap">
+            Dynmap
+          </Link>
+        </div>
+      </div>
+    </nav>
   </header>
 );
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,21 +2,24 @@ import React from "react";
 import { Link } from "gatsby";
 
 import Layout from "../components/layout";
-import Image from "../components/image";
 import SEO from "../components/seo";
 
 import "bulma";
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <SEO title="KINC Minecraft Server" />
+    <section className="hero is-dark is-bold is-fullheight">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">KINC Minecraft Server</h1>
+          <pre className="url">craft.ラムダ.com</pre>
+          <Link className="button" to="/dynmap">
+            Dynmap
+          </Link>
+        </div>
+      </div>
+    </section>
   </Layout>
 );
 
